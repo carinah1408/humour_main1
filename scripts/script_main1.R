32d5307a59e0290dde42d9f5b8b51055d7b15235
Carina Hoerst <carinahoerst@Carinas-MacBook-Pro.local>
2022-05-20 12:40:33 +0100

    manipulation checks

--- scripts/script_main1.R
+++ scripts/script_main1.R
@@ -3,6 +3,7 @@ library(tidyverse)
 library(dplyr)
 library(remotes)
 library(devtools)
+library(car)
 install_github("mdelacre/Routliers")
 
 ### load dataset, make political affilication and exp. conditions factors, recode political affiliation----
@@ -59,9 +60,59 @@ main1_sub %>%
   summarise(n())
 
 ### manipulation checks----
+## humour
 
-  
-  
+main1_sub %>%
+  filter(cond == "2") %>%
+  summarise(mean_hum_check1 = mean(hum_check1), sd_hum_check1 = sd(hum_check1),
+            mean_hum_check2 = mean(hum_check2), sd_hum_check2 = sd(hum_check2))
+
+## comparison of perceptions of free expression and organisation across conditions
+
+main1_sub %>%
+  group_by(cond) %>%
+  summarise(mean_eff_check1 = mean(eff_check1), sd_eff_check1 = sd(eff_check1), med_eff_check1 = median(eff_check1),
+            mean_eff_check2 = mean(eff_check2), sd_eff_check2 = sd(eff_check2), med_eff_check2 = median(eff_check2), 
+            n = n())
+
+# anova eff_check 1 (= "free expreesion")
+eff_1.aov <- aov(eff_check1 ~ cond, data = main1_sub)
+summary(eff_1.aov)
+
+TukeyHSD(eff_1.aov) # pairwise comparisons
+
+leveneTest(eff_check1 ~ cond, data = main1_sub) # checking homogeneity of variance: homogeneity of variance is violated, using Welch test instead:
+
+oneway.test(eff_check1 ~ cond, data = main1_sub)
+
+eff_1.pairwise.t.test <- pairwise.t.test(main1_sub$eff_check1, main1_sub$cond,
+                p.adjust.method = "BH", pool.sd = FALSE) # pairwise comparison (with no assumption of equal variances)
+eff_1.pairwise.t.test
+
+plot(eff_1.aov, 2) # checking normality: normality seems violated, therefore, non-parametric test:
+
+kruskal.test(eff_check1 ~ cond, data = main1_sub)
+
+eff_1.pairwise.wilcox.test <- pairwise.wilcox.test(main1_sub$eff_check1, main1_sub$cond,
+                     p.adjust.method = "BH", conf.int = TRUE) # pairwise comparison (non-parametric)
+eff_1.pairwise.wilcox.test
+
+# anova eff_check 2 (= "successful rally")
+eff_2.aov <- aov(eff_check2 ~ cond, data = main1_sub)
+summary(eff_2.aov)
+
+TukeyHSD(eff_2.aov) # pairwise comparisons
+
+leveneTest(eff_check2 ~ cond, data = main1_sub) # checking homogeneity of variance: no viiolation
+
+plot(eff_2.aov, 2) # checking normality: normality seems violated, therefore, non-parametric test:
+
+kruskal.test(eff_check2 ~ cond, data = main1_sub)
+
+eff_2.pairwise.wilcox.test <- pairwise.wilcox.test(main1_sub$eff_check2, main1_sub$cond,
+                                                   p.adjust.method = "BH", conf.int = TRUE) # pairwise comparison (non-parametric)
+eff_2.pairwise.wilcox.test
+
 
 
 
